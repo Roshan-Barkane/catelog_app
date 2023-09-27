af6a850ac31e9e410ce59717b31c45c2a3880326
Roshan-Barkane <roshanbaekhane987@gmail.com>
2023-09-27 12:13:17 +0530

    commit 3 task

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:appstore/home_page.dart';
+import 'package:appstore/pages/home_page.dart';
+import 'package:appstore/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
